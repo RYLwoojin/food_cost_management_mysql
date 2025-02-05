d3672dc7279ad5f38191df0a3eb016cdb3e54e66
Woojin LIM, Ryan <88407891+RYLwoojin@users.noreply.github.com>
2025-02-05 14:22:01 -0800

    Delete recipe_202502051420.sql

--- recipe_202502051420.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO menu_cost_calc.recipe (r_id,r_name) VALUES
-	 (1,'ribeye steak v1'),
-	 (2,'caesar salad'),
-	 (3,'blt sandwich'),
-	 (4,'tomato pasta v1'),
-	 (5,'carbonara pasta v1'),
-	 (6,'mushroom risotto'),
-	 (7,'tomato pasta v2');
