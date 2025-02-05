52469144bb57a7d800333de47a2630fa9136eb5a
Woojin LIM, Ryan <88407891+RYLwoojin@users.noreply.github.com>
2025-02-05 14:20:46 -0800

    Add files via upload

---
+++ ing_list_202502051418.sql
@@ -0,0 +1,22 @@
+INSERT INTO menu_cost_calc.ing_list (ing_id,ing_name,vendor_id,ing_pkg_price,ing_pkg_weight,unit_price) VALUES
+	 (1,'romain lettuce',7,35,50,1),
+	 (2,'parmesan cheese',2,120,10,12),
+	 (3,'bread',3,20,10,2),
+	 (4,'peanut oil',4,10,1,10),
+	 (5,'olive oil',4,15,1,15),
+	 (6,'ribeye',5,400,10,40),
+	 (7,'bacon',6,3,25,0),
+	 (8,'tomato',7,7,1,7),
+	 (9,'spaghetti',8,30,10,3),
+	 (10,'arborio rice',8,20,1,20);
+INSERT INTO menu_cost_calc.ing_list (ing_id,ing_name,vendor_id,ing_pkg_price,ing_pkg_weight,unit_price) VALUES
+	 (11,'shiitake mushroom',7,60,1,60),
+	 (12,'oyster mushroom',7,40,1,40),
+	 (13,'egg',10,50,2,25),
+	 (14,'pecorino cheese',2,140,10,14),
+	 (15,'chicken',10,50,3,17),
+	 (16,'carrot',7,12,1,12),
+	 (17,'onion',7,15,1,15),
+	 (18,'celery',7,9,1,9),
+	 (19,'garlic',7,17,3,6),
+	 (20,'herb mix',1,43,2,22);
