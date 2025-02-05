bf1e7225773f8fe448732e8d1c9503f33ada7c3d
Woojin LIM, Ryan <88407891+RYLwoojin@users.noreply.github.com>
2025-02-05 14:21:42 -0800

    Delete menu_detail_202502051419.sql

--- menu_detail_202502051419.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO menu_cost_calc.menu_detail (m_id,r_id,m_cost,m_selling_price) VALUES
-	 (1,1,23,37.52),
-	 (2,2,2,2.928),
-	 (3,3,1,2.24),
-	 (4,7,6,10.24),
-	 (5,5,13,20.16),
-	 (6,6,19,31.04);
