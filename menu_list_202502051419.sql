b833466660eb0737d9a53ceb82019de29577a06d
Woojin LIM, Ryan <88407891+RYLwoojin@users.noreply.github.com>
2025-02-05 14:21:50 -0800

    Delete menu_list_202502051419.sql

--- menu_list_202502051419.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO menu_cost_calc.menu_list (m_id,m_name) VALUES
-	 (1,'ribeye steak'),
-	 (2,'caesar salad'),
-	 (3,'blt sandwich'),
-	 (4,'tomato pasta'),
-	 (5,'carbonara pasta'),
-	 (6,'mushroom risotto');
